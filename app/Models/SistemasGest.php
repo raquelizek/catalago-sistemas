9d38d200c6514035e1e4f3c9a71605f738f41a6a
Raquel Ribeiro Reclizek <raquelreclizek@gmail.com>
2021-06-21 16:38:12 -0300

    refactor: removing unless lines

--- app/Models/SistemasGest.php
+++ app/Models/SistemasGest.php
@@ -12,27 +12,15 @@ class SistemasGest extends Model
 {
 
 
-    protected $fillable = ['dev', 'atuacao', 'auth', 'diretsi', 'sistnm', 'sissig', 'endereco', 'dns', 'pdti', 'gac', 'gds', 'gnt', 'siapegnt', 'gns', 'siapegns', 'git', 'siapegit', 'gis', 'siapegis', 'prsei', 'numerosei', 'obsv'];
+    protected $fillable = ['dev', 'atuacao', 'auth', 'diretsi', 'sistnm', 'sissig', 'endereco', 'status', 'dns', 'pdti', 'gac', 'gds', 'gnt', 'siapegnt', 'gns', 'siapegns', 'git', 'siapegit', 'gis', 'siapegis', 'prsei', 'numerosei', 'obsv'];
     public $table = 'sistemasgestao';
 }
 
 
-//último código adicionado 10/06 para criar a tabela se não existir e os campos!!!!!!!!!!
-$db_Host = "127.0.0.1";
-$db_username = 'root';
-$db_password = '';
-$db_primaryDatabase = 'catalogosistemas';
-
-
-$conn = new mysqli($db_Host, $db_username, $db_password, $db_primaryDatabase);
-if (mysqli_connect_errno()) {
-    printf("Não foi possível conectar ao banco de dados: %s\n", mysqli_connect_error());
-    exit();
-}
 
 if (!Schema::hasTable('sistemasgestao')) {
     Schema::create('sistemasgestao', function ($table) {
-        $table->increments('id')->primary();
+        $table->increments('sist_id')->primary();
         $table->timestamps();
         $table->string('dev');
         $table->string('atuacao');
--- database/migrations/2021_05_31_174716_create_sistemasgestao_table.php
+++ database/migrations/2021_05_31_174716_create_sistemasgestao_table.php
@@ -14,7 +14,7 @@ class CreateSistemasgestaoTable extends Migration
     public function up()
     {
         Schema::create('sistemasgestao', function (Blueprint $table) {
-            $table->id();
+            $table->increments('sist_id')->primary();
             $table->timestamps();
             $table->string('dev');
             $table->string('atuacao');
@@ -22,21 +22,22 @@ class CreateSistemasgestaoTable extends Migration
             $table->string('diretsi');
             $table->string('sistnm');
             $table->string('sissig');
-            $table->string('endereco');
-            $table->string('dns')->nullable();
-            $table->string('pdti')->nullable();
-            $table->string('gac')->nullable();
-            $table->string('gds')->nullable();
-            $table->string('gnt')->nullable();
-            $table->string('siapegnt')->unique();
-            $table->string('gns')->nullable();
-            $table->string('siapegns')->unique();
-            $table->string('git')->nullable();
-            $table->string('siapegit')->unique();
-            $table->string('gis')->nullable();
-            $table->string('siapegis')->unique();
-            $table->string('prsei')->nullable();
-            $table->string('numerosei')->nullable();
+            $table->string('endereco', 60);
+            $table->boolean('status')->default(0);
+            $table->boolean('dns')->default(0);
+            $table->boolean('pdti')->default(0);
+            $table->string('gac');
+            $table->string('gds');
+            $table->string('gnt');
+            $table->string('siapegnt', 7)->nullable();
+            $table->string('gns');
+            $table->string('siapegns', 7)->nullable();
+            $table->string('git');
+            $table->string('siapegit', 7)->nullable();
+            $table->string('gis');
+            $table->string('siapegis', 7)->nullable();
+            $table->boolean('prsei')->default(0);
+            $table->string('numerosei', 30)->unique();
             $table->text('obsv')->nullable();
         });
     }
