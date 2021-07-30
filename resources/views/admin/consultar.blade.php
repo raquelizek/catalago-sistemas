a762cbc4459032aded4b7fa76f6b9e2fe261200c
Raquel Ribeiro Reclizek <raquelreclizek@gmail.com>
2021-07-30 12:39:43 -0300

    refactor: update

--- resources/views/admin/consultar.blade.php
+++ resources/views/admin/consultar.blade.php
@@ -11,62 +11,63 @@ Consultar Sistemas
 <br>
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-md-14">
 
-        <div class="card">
-            <div class="card-body">
-                <div class="card-header" class="card-title text-uppercase text-default mb-0 ">
-                    <h2 class="card-title text-uppercase text-default mb-0 ">Consultar Sistemas</h2>
-                    <br>
-                    <div class="col-7 text-left">
-                        <a href="cadastrar-sistemas" class="btn btn-sm btn-outline-info text-info">Cadastrar Novo Sistema</a>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="card-body">
-                            @if (session('status'))
-                            <div class="alert alert-primary" role="alert">
-                                {{ session('status') }}
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-header" class="card-title text-uppercase text-default mb-0 ">
+                        <h2 class="card-title text-uppercase text-default mb-0 ">Consultar Sistemas</h2>
+                        <br>
+                        <div class="col-7 text-left">
+                            <a href="cadastrar-sistemas" class="btn btn-sm btn-outline-info text-info">Cadastrar Novo Sistema</a>
+                        </div>
+                        <div class="col-sm-5">
+                            <div class="card-body">
+                                @if (session('status'))
+                                <div class="alert alert-primary" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                                @endif
                             </div>
-                            @endif
                         </div>
                     </div>
-                </div>
-                <br>
-                <div class="row justify-content-center align-items-center">
-                    <h3>Sistemas Cadastrados</h3>
-                </div>
-                <br>
-                <div class="table-responsive ">
-                    <table class="table">
-                        <thead class=" text-black">
-                            <tr>
-                                <th><b>Nome do Sistema</b></th>
-                                <th><b>Endereço do Sistema</b></th>
-                                <th><b>Área Demandante</b></th>
-                                <th><b>Status</b></th>
-                                <th><b>Informações Técnicas</b></th>
-                                <th><b>SEI</b></th>
-                            </tr>
+                    <br>
+                    <div class="row justify-content-center align-items-center">
+                        <h3>Sistemas Cadastrados</h3>
+                    </div>
+                    <br>
+                    <div class="table-responsive ">
+                        <table class="table">
+                            <thead class=" text-black">
+                                <tr>
+                                    <th><b>Nome do Sistema</b></th>
+                                    <th><b>Endereço do Sistema</b></th>
+                                    <th><b>Área Demandante</b></th>
+                                    <th><b>Status</b></th>
+                                    <th><b>Informações Técnicas</b></th>
+                                    <th><b>SEI</b></th>
+                                </tr>
 
-                        </thead>
-                        <tbody>
-                            @foreach ($sistemas as $row) 
-                            
-                            <tr>
-                                <td><a href="/consultar-sist/{{$row->sistemasID}}"><b>{{$row->sistnm}}</b></a></td>
-                                <td>{{@$row->endereco}}</td>
-                                <td>{{@$row->diretsi}}</td>
-                                <td>{{@$row->status}}</td>
-                                <td><a href="/info-tec/{{$row->sistemasID}}" class="badge {{@$row->ambiente_hospedagem==''? 'badge-danger' : 'badge-success'}}">Informações Técnicas</a></td>
-                                <td><a href="#" class="badge badge-info">Processo do SEI</a></td>
-                                @endforeach
-                            </tr>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($sistemas as $row)
+
+                                <tr>
+                                    <td><a href="/consultar-sist/{{$row->sistemasID}}"><b>{{$row->sistnm}}</b></a></td>
+                                    <td>{{@$row->endereco}}</td>
+                                    <td>{{@$row->diretsi}}</td>
+                                    <td>{{@$row->status}}</td>
+                                    <td><a href="/info-tec/{{$row->sistemasID}}" class="badge {{@$row->ambiente_hospedagem==''? 'badge-danger' : 'badge-success'}}">Informações Técnicas</a></td>
+                                    <td><a href="/cadastar-sistemas/{{$row->sistemasID}}" class="badge badge-info">Processo do SEI</a></td>
+                                    @endforeach
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </div>
 
 
