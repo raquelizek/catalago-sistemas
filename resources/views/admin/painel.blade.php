509323ab245daaf4e862f89f253f43b7b0975847
Raquel Ribeiro Reclizek <raquelreclizek@gmail.com>
2021-08-12 14:41:41 -0300

    refactor: update

--- resources/views/admin/painel.blade.php
+++ resources/views/admin/painel.blade.php
@@ -7,6 +7,66 @@ Consultar Sistemas
 
 
 @section('content')
+<br>
+<div class="container-mt-8">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card" style="width: 1520px;">
+                <div class="card-header border-0" class="card-title text-uppercase text-default mb-0 ">
+                    <h2 class="card-title text-uppercase text-default mb-0 ">Prioridades de Sistemas</h2>
+                </div>
+                <hr class="my-0">
+                <div class="card-body">
+                    <canvas id="myChart" width="600" height="400"></canvas>
+                    <script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-8216c69d01441f36c0ea791ae2d4469f0f8ff5326f00ae2d00e4bb7d20e24edb.js"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
+                    <script id="rendered-js">
+                        var data = [{
+                                value: 300,
+                                color: "#F7464A",
+                                highlight: "#FF5A5E",
+                                label: "Red"
+                            },
+
+                            {
+                                value: 50,
+                                color: "#00BFFF",
+                                highlight: "#1E90FF	",
+                                label: "Blue"
+                            },
+
+                            {
+                                value: 100,
+                                color: "#FDB45C",
+                                highlight: "#FFC870",
+                                label: "Yellow"
+                            }
+                        ];
+                        var ctx = document.getElementById("myChart").getContext("2d");
+                        new Chart(ctx).Pie(data);
+                    </script>
+
+                    <div id="chartPreferences" class="ct-chart ct-perfect-fourth"></div>
+                    <br>
+                    <div class="legend">
+                        <i class="fa fa-circle text-danger"></i> {{ __('Prioridade 1') }}
+                        <br>
+                        <i class="fa fa-circle text-warning"></i> {{ __('Prioridade 2') }}
+                        <br>
+                        <i class="fa fa-circle text-info"></i> {{ __('Prioridade 3 e 4') }}
+
+
+                    </div>
+                    <hr>
+                    <div class="stats">
+                        <i class="fa fa-clock-o"></i> {{ __('Atualizado 2 dias atrás') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
 
 
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,173 +20,174 @@
     <!-- Argon CSS -->
     <link rel="stylesheet" href="/assets/css/argon.css?v=1.2.0" type="text/css">
 </head>
+
 <body>
-<!-- Sidenav -->
-<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
-    <div class="scrollbar-inner">
-        <!-- Brand -->
-        <div class="sidenav-header  align-items-center">
-        <a class="navbar-brand pt-4.5" href="/dashboard">
+    <!-- Sidenav -->
+    <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+        <div class="scrollbar-inner">
+            <!-- Brand -->
+            <div class="sidenav-header  align-items-center">
+                <a class="navbar-brand pt-4.5" href="/dashboard">
                     Catálogo de Sistemas
                 </a>
-        </div>
-        <hr class="my-0">
-        <div class="navbar-inner">
-            <!-- Collapse -->
-            <div class="collapse navbar-collapse" id="sidenav-collapse-main">
-                <!-- Nav items -->
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/dashboard">
-                            <i class="fas fa-brain text-green"></i>
-                            <span class="nav-link-text">Página Inicial</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
-                            <i class="fas fa-chalkboard text-blue"></i>
-                            <span class="nav-link-text">Sistemas</span>
-                        </a>
-
-                        <div class="collapse" id="navbar-examples">
-                            <ul class="nav nav-sm flex-column">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/consultar">
-                                        Catálogo de Sistemas
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/consultar">
-                                        Gestão de Sistemas
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link" href="/usuarios-registrados">
-                            <i class="fas fa-chalkboard-teacher text-orange"></i>
-                            <span class="nav-link-text">Permissões</span>
-                        </a>
-                    </li>
-                </ul>
-                <!-- Divider -->
-                <hr class="my-3">
-                <!-- Heading -->
-                <h6 class="navbar-heading p-0 text-muted">
-                    <span class="docs-normal">Prioridades</span>
-                </h6>
-                <!-- Navigation -->
-                
-                <ul class="navbar-nav mb-md-3">
-                <li class="nav-item ">
-                        <a class="nav-link" href="/painel">
-                            <i class="fas fa-chart-bar text-green"></i>
-                            <span class="nav-link-text">Painel</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html" target="_blank">
-                            <i class="far fa-circle text-danger"></i>
-                            <span class="nav-link-text">Urgente</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html" target="_blank">
-                            <i class="far fa-circle text-warning"></i>
-                            <span class="nav-link-text">Atenção</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html" target="_blank">
-                            <i class="far fa-circle text-primary"></i>
-                            <span class="nav-link-text">Informal</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/plugins/charts.html" target="_blank">
-                            <i class="fas fa-box-open"></i>
-                            <span class="nav-link-text">Requisitos</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/plugins/charts.html" target="_blank">
-                            <i class="far fa-address-card"></i>
-                            <span class="nav-link-text">Responsáveis</span>
-                        </a>
-                    </li>
-                </ul>
-
             </div>
-        </div>
-    </div>
-</nav>
-<div class="main-content" id="panel">
-    <!-- Topnav -->
-    <nav class="navbar navbar-top navbar-expand navbar-dark bg-white border-bottom">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Search form -->
-                <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
-                    <div class="form-group mb-0">
-                        <div class="input-group input-group-alternative input-group-merge">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-search"></i></span>
-                            </div>
-                            <input class="form-control" name="pesquisar" placeholder="Pesquisar" type="text">
-                        </div>
-                    </div>
-                    <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </form>
-                <!-- Navbar links -->
-                <ul class="navbar-nav align-items-center  ml-md-auto ">
-                    <li class="nav-item d-xl-none">
-                        <!-- Sidenav toggler -->
-                        <div class="pr-3 sidenav-toggler sidenav-toggler-dark" data-action="sidenav-pin" data-target="#sidenav-main">
-                            <div class="sidenav-toggler-inner">
-                                <i class="sidenav-toggler-line"></i>
-                                <i class="sidenav-toggler-line"></i>
-                                <i class="sidenav-toggler-line"></i>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="nav-item d-sm-none">
-                        <a class="nav-link" href="#" data-action="search-show" data-target="#navbar-search-main">
-                            <i class="ni ni-zoom-split-in"></i>
-                        </a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav align-items-center  ml-auto ml-md-0 ">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <div class="media align-items-center">
-                                <span class="avatar avatar-sm rounded-circle">
-                                    <img alt="Image placeholder" src="/assets/img/theme/usuarios-1.png">
-                                </span>
-                                <div class="media-body  ml-2  d-none d-lg-block">
-                                    <span class="mb-0 text-sm text-dark font-weight-bold">Usuário Administrador</span>
-                                </div>
-                            </div>
-                        </a>
-                        <div class="dropdown-menu  dropdown-menu-right ">
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                <i class="fas fa-door-open"></i>
-                                <span>Sair</span>
+            <hr class="my-0">
+            <div class="navbar-inner">
+                <!-- Collapse -->
+                <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+                    <!-- Nav items -->
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/dashboard">
+                                <i class="fas fa-brain text-green"></i>
+                                <span class="nav-link-text">Página Inicial</span>
                             </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
+                                <i class="fas fa-chalkboard text-blue"></i>
+                                <span class="nav-link-text">Sistemas</span>
+                            </a>
+
+                            <div class="collapse" id="navbar-examples">
+                                <ul class="nav nav-sm flex-column">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/consultar">
+                                            Catálogo de Sistemas
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/consultar">
+                                            Gestão de Sistemas
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="/usuarios-registrados">
+                                <i class="fas fa-chalkboard-teacher text-orange"></i>
+                                <span class="nav-link-text">Permissões</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- Divider -->
+                    <hr class="my-3">
+                    <!-- Heading -->
+                    <h6 class="navbar-heading p-0 text-muted">
+                        <span class="docs-normal">Prioridades</span>
+                    </h6>
+                    <!-- Navigation -->
+
+                    <ul class="navbar-nav mb-md-3">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="/painel">
+                                <i class="fas fa-chart-bar text-green"></i>
+                                <span class="nav-link-text">Painel</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html" target="_blank">
+                                <i class="far fa-circle text-danger"></i>
+                                <span class="nav-link-text">Urgente</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html" target="_blank">
+                                <i class="far fa-circle text-warning"></i>
+                                <span class="nav-link-text">Atenção</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html" target="_blank">
+                                <i class="far fa-circle text-primary"></i>
+                                <span class="nav-link-text">Informal</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/plugins/charts.html" target="_blank">
+                                <i class="fas fa-box-open"></i>
+                                <span class="nav-link-text">Requisitos</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/plugins/charts.html" target="_blank">
+                                <i class="far fa-address-card"></i>
+                                <span class="nav-link-text">Responsáveis</span>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
             </div>
         </div>
     </nav>
-</div>
-<hr class="my-0">
+    <div class="main-content" id="panel">
+        <!-- Topnav -->
+        <nav class="navbar navbar-top navbar-expand navbar-dark bg-white border-bottom">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Search form -->
+                    <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
+                        <div class="form-group mb-0">
+                            <div class="input-group input-group-alternative input-group-merge">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-search"></i></span>
+                                </div>
+                                <input class="form-control" name="pesquisar" placeholder="Pesquisar" type="text">
+                            </div>
+                        </div>
+                        <button type="button" class="close" data-action="search-close" data-target="#navbar-search-main" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </form>
+                    <!-- Navbar links -->
+                    <ul class="navbar-nav align-items-center  ml-md-auto ">
+                        <li class="nav-item d-xl-none">
+                            <!-- Sidenav toggler -->
+                            <div class="pr-3 sidenav-toggler sidenav-toggler-dark" data-action="sidenav-pin" data-target="#sidenav-main">
+                                <div class="sidenav-toggler-inner">
+                                    <i class="sidenav-toggler-line"></i>
+                                    <i class="sidenav-toggler-line"></i>
+                                    <i class="sidenav-toggler-line"></i>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item d-sm-none">
+                            <a class="nav-link" href="#" data-action="search-show" data-target="#navbar-search-main">
+                                <i class="ni ni-zoom-split-in"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav align-items-center  ml-auto ml-md-0 ">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <div class="media align-items-center">
+                                    <span class="avatar avatar-sm rounded-circle">
+                                        <img alt="Image placeholder" src="/assets/img/theme/usuarios-1.png">
+                                    </span>
+                                    <div class="media-body  ml-2  d-none d-lg-block">
+                                        <span class="mb-0 text-sm text-dark font-weight-bold">Usuário Administrador</span>
+                                    </div>
+                                </div>
+                            </a>
+                            <div class="dropdown-menu  dropdown-menu-right ">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    <i class="fas fa-door-open"></i>
+                                    <span>Sair</span>
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <hr class="my-0">
 
     @yield('content')
 
@@ -203,5 +204,8 @@
     <script src="/assets/vendor/chart.js/dist/Chart.extension.js"></script>
     <!-- Argon JS -->
     <script src="/assets/js/argon.js?v=1.2.0"></script>
+    <script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-8216c69d01441f36c0ea791ae2d4469f0f8ff5326f00ae2d00e4bb7d20e24edb.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
 </body>
+
 </html>
